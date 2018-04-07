82db2081c35414d05a24f5e369722cc2d9c908f5
rita <m.tsepiennikova@mail.com>
2018-04-07 14:37:51 +0300

    edited

--- src/Homework4_3.java
+++ src/Homework4_3.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 
 public class Homework4_3 {
     public static void main(String[] args) {
-        int[] list = new int [100];
-        for (int i = list.length; i <= 0; i--) {
+        int[] list = new int [101];
+        for (int i = list.length-1; i >= 0; i--) {
             list[i] = i;
         }
         System.out.println(Arrays.toString(list));
