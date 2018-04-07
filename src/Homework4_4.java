4cb9bf4d7c71ea3e199b252bd5cb7edf6dfcf654
rita <m.tsepiennikova@mail.com>
2018-04-07 14:53:12 +0300

    edited

--- src/Homework4_4.java
+++ src/Homework4_4.java
@@ -4,13 +4,15 @@
 
 public class Homework4_4 {
     public static void main(String[] args) {
-        int[] list = {45, -78, 8, 78, 65, 777, 9, -2, 32, -714};
+        int[] list = {1, -78, 8, 78, 65, 777, 9, -2, 32, -714};
         int sum = 0;
         for (int i = 0; i < list.length-1; i++) {
-            if ((i != 0) && (i != list.length-1)){
+            if (i != 0 && i!= list.length-1) {
                 sum += list[i];
             }
         }
         System.out.println(sum);
     }
 }
+
+
