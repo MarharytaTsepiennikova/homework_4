82fd7f6e4a1862ef0746d1cc5ef13fa1b685f759
rita <m.tsepiennikova@mail.com>
2018-04-08 11:21:14 +0300

    edited

--- src/Homework4_7.java
+++ src/Homework4_7.java
@@ -4,9 +4,16 @@
 public class Homework4_7 {
     public static void main(String[] args) {
         int[] list = {3, 54, 6, -8, 4, -8, 1, -8};
-        int rez = 0;
+        int n = 0, m = 0, rez = 0;
         for (int i = 0; i < list.length; i++) {
-
+            for (int j = 0; j < list.length; j++) {
+                if (list[i] == list[j]){
+                    n++;
+                }
+                if (n > m);
+                m = n;
+                rez = list[i];
+            }
         }
         System.out.println(rez);
     }
