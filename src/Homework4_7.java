58175cd8990f2161b3c6d477c106eb5c96252447
rita <m.tsepiennikova@mail.com>
2018-04-10 12:40:34 +0300

    change name of variables

--- src/Homework4_7.java
+++ src/Homework4_7.java
@@ -4,14 +4,14 @@
 public class Homework4_7 {
     public static void main(String[] args) {
         int[] list = {3, 54, 6, -8, 4, -8, 1, -8};
-        int n = 0, m = 0, rez = 0;
+        int var = 0, num = 0, rez = 0;
         for (int i = 0; i < list.length; i++) {
             for (int j = 0; j < list.length; j++) {
                 if (list[i] == list[j]){
-                    n++;
+                    var++;
                 }
-                if (n > m);
-                m = n;
+                if (var > num);
+                num = var;
                 rez = list[i];
             }
         }
